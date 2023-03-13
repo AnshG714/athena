da7d451980bb0e150a25eafb4807bd3d6463d4cf
Ansh Godha <anshgodha714@gmail.com>
2023-03-13 15:11:42 -0700

    Basic history endpoint

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-from flask import Flask
+from flask import Flask, request
+from summarizer import Summarizer
+from timeline_extractor import TimelineExtractor
+import asyncio
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "Server Works!"
+@app.post("/api/v1/history/")
+async def gen_history_content():
+    body = request.json
 
+    if not body:
+        return {"error": "Missing request body"}, 400
 
-@app.route("/greet")
-def say_hello():
-    return "Hello from Server"
+    file_source = body.get("source", None)
+    if not file_source:
+        return {"error": "Missing file source"}, 400
+
+    summarizer = Summarizer()
+    summarizer.load_text_from_file(f"./{file_source}")
+
+    timeline_extractor = TimelineExtractor()
+    timeline_extractor.load_text_from_file(f"./{file_source}")
+
+    results = await asyncio.gather(
+        *[summarizer.get_results(), timeline_extractor.get_results()]
+    )
+
+    summary = results[0]
+    timeline = results[1]
+    return {"summary": summary, "timeline": timeline}
