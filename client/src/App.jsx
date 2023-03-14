b8d721a4094b57e8aec5c98af01a95355610377c
Ansh Godha <anshgodha714@gmail.com>
2023-03-14 12:58:11 -0700

    Basic routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,53 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const SUBJECTS = ["History", "Chemistry", "Computer Science"];
 
 function SubjectCard({ subjectName }) {
+  const link = `/${subjectName.toLowerCase().replace(" ", "-")}`;
   return (
-    <button
-      className="SubjectCard"
-      style={{
-        border: "none",
-        cursor: "pointer",
-        outline: "none",
-        fontSize: 20,
-      }}
-    >
-      {subjectName}
-    </button>
+    <Link to={link}>
+      <button
+        className="SubjectCard"
+        style={{
+          border: "none",
+          cursor: "pointer",
+          outline: "none",
+          fontSize: 20,
+        }}
+      >
+        {subjectName}
+      </button>
+    </Link>
   );
 }
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <h3 style={{ textAlign: "center", fontWeight: "normal" }}>
         Welcome! Please select a subject to get started.
       </h3>
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <SubjectCard subjectName="History" />
-        <SubjectCard subjectName="History" />
-        <SubjectCard subjectName="History" />
+        {SUBJECTS.map((subject) => (
+          <SubjectCard subjectName={subject} />
+        ))}
       </div>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/history" element={<div>History page</div>} />
+        <Route path="/computer-science" element={<div>CS page</div>} />
+        <Route path="/chemistry" element={<div>Chemistry page</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
